24de6db730b37be2da7fc6a53773a25a3a1af67e
Dmytro Yesyp <dmytro.yesyp.knm.2020@lpnu.ua>
2021-11-11 22:09:09 +0200

    lab_7

--- DB/model.py
+++ DB/model.py
@@ -6,11 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 import enum
 import sys
 
-sys.path.append(r"/home/lyubomur/PycharmProjects/laab_6")
+#sys.path.append(r"/home/lyubomur/PycharmProjects/laab_6")
 
-DB_URI = os.getenv("DB_URI","postgresql://postgres:1462357980@localhost:5432/laab_6")
-
-engine = create_engine(DB_URI)
+engine = create_engine("mysql+pymysql://root:root@127.0.0.1/pythonbd")
 
 SessionFactory = sessionmaker(bind=engine)
 
@@ -34,31 +32,31 @@ class RequestEnum(enum.Enum):
 class users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer,primary_key=True)
-    username = Column(String)
-    firstName = Column(String)
-    lastName = Column(String)
-    email = Column(String)
-    password = Column(String)
-    phone = Column(String)
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    username = Column(String(100))
+    firstName = Column(String(100))
+    lastName = Column(String(100))
+    email = Column(String(100))
+    password = Column(String(100))
+    phone = Column(String(100))
+    userType = Column(String(100))
 
 
 
 class course(Base):
     __tablename__ = 'course'
-
-    id = Column(Integer,primary_key=True)
-    courseName = Column(String)
-    courseDescription = Column(String)
-
+    id = Column(Integer(),primary_key=True)
+    courseName = Column(String(100))
+    courseDescription = Column(String(100))
 
 
-class request(Base):
+
+class requestClass(Base):
     __tablename__ = 'request'
-    id = Column(Integer, primary_key=True)
-    requestFrom = Column(Integer,ForeignKey(users.id))
-    requestToCourse = Column(Integer,ForeignKey(course.id))
-    requestToLector = Column(Integer,ForeignKey(users.id))
+    id = Column(Integer(), primary_key=True,autoincrement=True)
+    requestFrom = Column(Integer(),ForeignKey(users.id))
+    requestToCourse = Column(Integer(),ForeignKey(course.id))
+    requestToLector = Column(Integer(),ForeignKey(users.id))
 
 user1 = users(username="username1",firstName="firstname1",lastName="lastName1",email="email1",password="password1",phone="phone1")
 user2 = users(username="username2",firstName="firstname2",lastName="lastName2",email="email2",password="password2",phone="phone2")
@@ -69,8 +67,8 @@ user5 = users(username="username5",firstName="firstname5",lastName="lastName5",e
 course1 = course(courseName="courseName1",courseDescription="courseDescription1")
 course2 = course(courseName="courseName2",courseDescription="courseDescription2")
 
-request1 = request(requestFrom=1,requestToCourse=1,requestToLector=1)
-request2 = request(requestFrom=2,requestToCourse=2,requestToLector=2)
+request1 = requestClass(requestFrom=1,requestToCourse=1,requestToLector=1)
+request2 = requestClass(requestFrom=2,requestToCourse=2,requestToLector=2)
 
 
 # Session.add(user1)
@@ -80,8 +78,8 @@ request2 = request(requestFrom=2,requestToCourse=2,requestToLector=2)
 # Session.add(user5)
 # Session.add(course1)
 # Session.add(course2)
-Session.add(request1)
-Session.add(request2)
+# Session.add(request1)
+# Session.add(request2)
 
 
-Session.commit()
+# Session.commit()
