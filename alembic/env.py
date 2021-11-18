2c265ce00e1a1efca2bf60c56ef683026e7bf40f
DmytroYesyp <91474410+DmytroYesyp@users.noreply.github.com>
2021-11-18 10:35:13 +0200

    7 lab (#9)
    * lab_7
    
    * lab_7
    
    * lab_7

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from model import Base
+from DB.model import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
