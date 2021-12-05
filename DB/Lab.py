aed4570e5f7baf32285cc50abce7922d08b9581a
temp <artemust5@gmail.com>
2021-12-05 20:08:22 +0200

    lab8

--- DB/Lab.py
+++ DB/Lab.py
@@ -1,244 +1,291 @@
+from functools import wraps
+
 from flask_bcrypt import Bcrypt
-from flask import Flask, Response
-from model import *
-from flask import jsonify
-import json
-from flask import make_response
-from flask import request
-from sqlalchemy.exc import IntegrityError
+from flask_httpauth import HTTPBasicAuth
+from marshmallow import ValidationError
+from sqlalchemy.orm import sessionmaker
+from flask import jsonify, request, Blueprint
+from DB.tables import *
+from schema import *
 
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
+session = sessionmaker(bind=engine)
+s = session()
 
-def to_json(inst, cls):
-    """
-    Jsonify the sql alchemy query result.
-    """
-    convert = dict()
-    d = dict()
-    for c in cls.__table__.columns:
-        v = getattr(inst, c.name)
-        if c.type in convert.keys() and v is not None:
-            try:
-                d[c.name] = convert[c.type](v)
-            except:
-                d[c.name] = "Error:  Failed to covert using ", str(convert[c.type])
-        elif v is None:
-            d[c.name] = str()
-        else:
-            d[c.name] = v
-    return json.dumps(d)
+bcrypt = Bcrypt()
+
+query = Blueprint("query", __name__)
+auth = HTTPBasicAuth()
 
 
+@auth.verify_password
+def verify_password(username, password):
 
-# ----------------------------------- user ------------------
+    user = s.query(User).filter(User.username == username).one_or_none()
+    if user is None:
+        return False
 
-@app.route('/user', methods=['POST'])
-def create_user():
-    print("create_user")
-    user = users(
-        username = request.json.get('username'),
-        firstName = request.json.get('firstName'),
-        lastName = request.json.get('lastName'),
-        email = request.json.get('email'),
-        password = bcrypt.generate_password_hash(request.json.get('password')).decode('utf-8'),
-        phone = request.json.get('email'),
-        userType = request.json.get('userType')
-    )
-    check = (Session.query(users).filter_by(username=user.username).all())
-    if check != []:
-        return make_response(jsonify({'error': 'User with such username already exists'}), 409)
+    if not bcrypt.check_password_hash(user.password, password):
+        return False
+
+    return user
+
+
+def permission_required(permission):
+    def decorator(func):
+        @wraps(func)
+        def decorated_function(*args, **kwargs):
+            if auth.current_user().userType != permission:
+                return {"message": "Forbidden"}, 403
+            return func(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+@query.route('/user/<string:username>', methods=['GET'])
+@auth.login_required
+def get_user(username):
+    if auth.username() != s.query(User).filter_by(username=username).first().username:
+        return {"message": "Forbidden"}, 403
+    user = s.query(User).filter(User.username == username).first()
+    if user is None:
+        return {"message": "User could not be found."}, 404
+    schema = UserSchema()
+    return schema.dump(user), 200
+
+
+@query.route('/user/<string:username>', methods=['PUT'])
+@auth.login_required
+def update_user(username):
+    if auth.username() != s.query(User).filter_by(username=username).first().username:
+        return {"message": "Forbidden"}, 403
+    params = request.json
+    if not params:
+        return {"message": "No input data provided"}, 400
+    if 'id' in params:
+        return {"message": "You can not change id"}, 400
+    user = s.query(User).filter(User.username == username).first()
+    if user is None:
+        return {"message": "User could not be found."}, 404
+    schema = UserSchema()
     try:
-        Session.add(user)
-        Session.commit()
-    except IntegrityError:
-        print('incorrect data')
-        return make_response(jsonify({'error': 'incorrect data'}), 409)
-    a = to_json(user, users)
-    return Response(response=a,
-                status=200,
-                mimetype="application/json")
+        data = schema.load(params)
+    except ValidationError as err:
+        return err.messages, 422
+    check_email = s.query(User).filter(User.email == request.json.get('email')).first()
+    check_username = s.query(User).filter(User.username == request.json.get('username')).first()
+    if check_email is not None and request.json.get('email') != user.email:
+        return {"message": "User with provided email already exists"}, 406
+    if check_username is not None and request.json.get('username') != username:
+        return {"message": "User with provided username already exists"}, 406
+    for key, value in params.items():
+        setattr(user, key, value)
+    hashed = Bcrypt().generate_password_hash(params['password']).decode('utf - 8')
+    user.password = hashed
+    s.commit()
+    return schema.dump(data), 200
 
 
-@app.route("/user/<int:id>", methods=['GET'])
-def get_user_info(id):
+@query.route('/user/<string:username>', methods=['DELETE'])
+@auth.login_required()
+def delete_user(username):
+    if auth.username() != s.query(User).filter_by(username=username).first().username:
+        return {"message": "Forbidden"}, 403
+    user = s.query(User).filter(User.username == username).first()
+    if user is None:
+        return {"message": "User could not be found."}, 404
+    s.query(CourseMember).filter(CourseMember.userId == user.id).delete(synchronize_session="fetch")
+    s.query(Request).filter(Request.requestToLector == user.id).delete(synchronize_session="fetch")
+    s.query(Request).filter(Request.studentId == user.id).delete(synchronize_session="fetch")
+    s.delete(user)
+    s.commit()
+    schema = UserSchema()
+    return schema.dump(user), 200
+
+
+@query.route('/user/acceptrequest/<int:request_id>', methods=['PUT'])
+@auth.login_required()
+@permission_required('Lector')
+def accept_request(request_id):
+    req = s.query(Request).filter(Request.id == request_id).first()
+    if req is None:
+        return {"message": "Request could not be found."}, 404
+    if s.query(CourseMember).filter(CourseMember.courseId == getattr(req, 'requestToCourse')).count() <= 5:
+        setattr(req, 'status', 'Accepted')
+        s.query(Request).filter(Request.id == request_id).delete(synchronize_session="fetch")
+        new_course_member = CourseMember(courseId=getattr(req, 'requestToCourse'), userId=getattr(req, 'studentId'))
+        s.add(new_course_member)
+        s.commit()
+    else:
+        return {"message": "Request can't be accepted because the limit of students will be exceeded."}, 406
+    return {"message": "Accepted"}, 200
+
+
+@query.route('/user/declinerequest/<int:request_id>', methods=['PUT'])
+@auth.login_required()
+@permission_required('Lector')
+def decline_request(request_id):
+    req = s.query(Request).filter(Request.id == request_id).first()
+    if req is None:
+        return {"message": "Request could not be found."}, 404
+    setattr(req, 'status', 'Declined')
+    s.commit()
+    return {"message": "Declined"}, 200
+
+
+@query.route('/course', methods=['POST'])
+@auth.login_required()
+@permission_required('Lector')
+def add_course():
+    new_course_json = request.json
+    if not new_course_json:
+        return {"message": "No input data provided"}, 400
+    if 'id' in new_course_json:
+        return {"message": "You can not change id"}, 400
+    schema = CourseSchema()
     try:
-        a = to_json(Session.query(users).filter_by(id=id).one(), users)
-        return Response(response=a,
-                    status=200,
-                    mimetype="application/json")
-    except:
-        return make_response(jsonify({'error': 'Not found'}), 404)
+        data = schema.load(new_course_json)
+    except ValidationError as err:
+        return err.messages, 422
+    user_lector = s.query(User).filter(User.id == request.json.get('courseLector')).first()
+    if user_lector is None:
+        return {"message": "User could not be found."}, 404
+    if user_lector.userType != 'Lector':
+        return {"message": "This user is not lector."}, 406
+    s.add(data)
+    s.commit()
+    return schema.dump(data), 200
 
 
+@query.route('/course/<int:course_id>', methods=['GET'])
+@auth.login_required()
+def get_course(course_id):
+    courses = s.query(Course).join(CourseMember).filter(CourseMember.userId == auth.current_user().id).all()
+    course = s.query(Course).filter(Course.id == course_id).first()
+    if course is None:
+        return {"message": "Course could not be found."}, 404
+    is_user_course = False
+    for course_iter in courses:
+        if course_id == course_iter.id:
+            is_user_course = True
+    if not is_user_course:
+        return {"message": "Forbidden"}, 403
+    schema = CourseSchema()
+    return schema.dump(course), 200
 
-@app.route('/user/<int:id>', methods=['PUT'])
-def update_user(id):
-    print("update_user")
-    z = Session.query(users).filter_by(id=id).all()
-    if not z:
-        return make_response(jsonify({'error': 'Not found'}), 404)
 
-    u = Session.query(users).filter_by(id=id).one()
-    if request.json.get('username'):
-        tvins = (Session.query(users).filter_by(username=request.json.get('username')).all())
-        if tvins != []:
-            return make_response(jsonify({'error': 'username is busy'}), 409)
-        u.username = request.json.get('username')
-    if request.json.get('firstName'):
-        u.firstName = request.json.get('firstName')
-    if request.json.get('lastName'):
-        u.lastName = request.json.get('lastName')
-    if request.json.get('password'):
-        u.password = request.json.get('password')
-    if request.json.get('phone'):
-        u.password = request.json.get('phone')
-    if request.json.get('userType'):
-        u.password = request.json.get('userType')
-    Session.commit()
-    Session.commit()
+# get courses for user with provided id
+@query.route('/user/course', methods=['GET'])
+@auth.login_required()
+@permission_required('Lector')
+def get_course_by_userid():
+    courses = s.query(Course).filter(Course.courseLector == auth.current_user().id).all()
+    if not courses:
+        return {"message": "Courses could not be found."}, 404
+    schema = CourseSchema()
+    return jsonify(schema.dump(courses, many=True)), 200
 
-    return Response(response=to_json(u, users),
-                    status=200,
-                    mimetype="application/json")
 
-@app.route('/user/<int:id>', methods=['DELETE'])
-def delete_user(id):
+@query.route('/course/<int:course_id>', methods=['PUT'])
+@auth.login_required()
+@permission_required('Lector')
+def update_course(course_id):
+    course = s.query(Course).filter(Course.id == course_id).first()
+    if course is None:
+        return {"message": "Course could not be found."}, 404
+    params = request.json
+    if not params:
+        return {"message": "No input data provided"}, 400
+    schema = CourseSchema()
     try:
-        user = Session.query(users).filter_by(id=id).first()
-        Session.delete(user)
-        Session.commit()
-        return {
-            "msg": "user deleted successfully",
-            "id": id
-        }
-    except:
-        return "user not found", 404
+        data = schema.load(params)
+    except ValidationError as err:
+        return err.messages, 422
+    user = s.query(User).filter(User.id == request.json.get('courseLector')).first()
+    if user is None:
+        return {"message": "User could not be found."}, 404
+    if user.userType != 'Lector':
+        return {"message": "This user is not lector."}, 406
+    for key, value in params.items():
+        setattr(course, key, value)
+    s.commit()
+    return schema.dump(data), 200
 
-# ------------------------------ course -------------------------
 
-@app.route('/course', methods=['POST'])
-def create_course():
-    print("create_course")
-    c = course(
-        id = request.json.get('id'),
-        courseName = request.json.get('courseName'),
-        courseDescription = request.json.get('courseDescription'),
-    )
-    tvins = (Session.query(course).filter_by(id=c.id).all())
-    if tvins != []:
-        return make_response(jsonify({'error': 'course with such id already exists'}), 409)
+@query.route('/course/<int:course_id>', methods=['DELETE'])
+@auth.login_required()
+@permission_required('Lector')
+def delete_course(course_id):
+    course = s.query(Course).filter(Course.id == course_id).first()
+    if course is None:
+        return {"message": "Course could not be found."}, 404
+    s.query(CourseMember).filter(CourseMember.courseId == course.id).delete(synchronize_session="fetch")
+    s.query(Request).filter(Request.requestToCourse == course.id).delete(synchronize_session="fetch")
+    s.delete(course)
+    s.commit()
+    schema = CourseSchema()
+    return schema.dump(course), 200
+
+
+@query.route('/request', methods=['POST'])
+@auth.login_required()
+@permission_required('Student')
+def add_request():
+    new_request = request.json
+    if not new_request:
+        return {"message": "No input data provided"}, 400
+    if 'id' in new_request:
+        return {"message": "You can not change id"}, 400
+    schema = RequestSchema()
     try:
-        Session.add(c)
-        Session.commit()
-    except IntegrityError:
-        print('incorrect data')
-        return make_response(jsonify({'error': 'incorrect data'}), 409)
-    # tasks.append(task)
-    a = to_json(c, course)
-    return Response(response=a,
-                status=200,
-                mimetype="application/json")
+        data = schema.load(new_request)
+    except ValidationError as err:
+        return err.messages, 422
+    if new_request['studentId'] != auth.current_user().id:
+        return {"message": "Forbidden"}, 403
+    user_lector = s.query(User).filter(User.id == request.json.get('requestToLector')).first()
+    if user_lector is None:
+        return {"message": "Lector could not be found."}, 404
+    if user_lector.userType != 'Lector':
+        return {"message": "This user is not a lector."}, 406
+    user_student = s.query(User).filter(User.id == request.json.get('studentId')).first()
+    if user_student is None:
+        return {"message": "Student could not be found."}, 404
+    if user_student.userType != 'Student':
+        return {"message": "This user is not a student."}, 406
+    course = s.query(Course).filter(Course.id == request.json.get('requestToCourse')).first()
+    request_to_join = s.query(Request).filter(Request.id == request.json.get('requestToCourse')).all()
+    if len(request_to_join) == 1:
+        return {"message": "Request was already sent."}, 406
+    if course is None:
+        return {"message": "Course could not be found."}, 404
+    if course.courseLector != user_lector.id:
+        return {"message": "Provided lector is not allowed to accept this request."}, 406
+    if course.courseLector != user_lector.id:
+        return {"message": "Provided lector is not allowed to accept this request."}, 406
+    s.add(data)
+    s.commit()
+    return schema.dump(data), 200
 
 
-
-@app.route("/course/getAll/<int:id>", methods=['GET'])
-def all_courses_for_user(id):
-    z = Session.query(users).filter_by(id=id).all()
-    if not z:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-
-    a = Session.query(requestClass).filter_by(requestFrom=id).all()
-    b = []
-    for i in a:
-        b.append(Session.query(course).filter_by(id=i.requestToCourse).one())
-
-    json_data = []
-    for i in b:
-        json_data.append(to_json(i, course))
+@query.route('/auth/register', methods=['POST'])
+def add_user():
+    new_user_json = request.json
+    if not new_user_json:
+        return {"message": "No input data provided"}, 400
+    if 'id' in new_user_json:
+        return {"message": "You can not change id"}, 400
+    schema = UserSchema()
     try:
-        Session.commit()
-    except:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-
-    return Response(response=str(json_data),
-                    status=200,
-                    mimetype="application/json")
-
-@app.route("/course/get/<int:id>", methods=['GET'])
-def get_course_ById(id):
-    try:
-        a = to_json(Session.query(course).filter_by(id=id).one(), course)
-        return Response(response=a,
-                    status=200,
-                    mimetype="application/json")
-    except:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@app.route('/course/update/<int:id>', methods=['PUT'])
-def update_course(id):
-    print("update_course")
-    z = Session.query(users).filter_by(id=id).all()
-    if not z:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-    c = Session.query(course).filter_by(id=id).one()
-    if not c:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-    if request.json.get('courseName'):
-        c.courseName = request.json.get('courseName')
-    if request.json.get('courseDescription'):
-        c.courseDescription = request.json.get('courseDescription')
-    Session.commit()
-    return Response(response=to_json(c, course),
-                status=200,
-                mimetype="application/json")
-
-
-@app.route('/course/<int:id>', methods=['DELETE'])
-def delete_course(id):
-    u = Session.query(course).filter_by(id=id).all()
-    if not u:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-    try:
-        cu = Session.query(course).filter_by(id=id).first()
-        Session.delete(cu)
-        Session.commit()
-        return {
-            "msg": "course deleted successfully",
-            "id": id
-        }
-    except:
-        return "course has foreign keys in other table", 405
-
-# ----------------------- request -----------------
-
-@app.route('/request', methods=['POST'])
-def create_request():
-    req = requestClass(
-        requestFrom = request.json.get('requestFrom'),
-        requestToCourse = request.json.get('requestToCourse'),
-        requestToLector = request.json.get('requestToLector'),
-    )
-
-    Check2 = (Session.query(requestClass).filter_by(requestToCourse=req.requestToCourse).all())
-    if Check2 != []:
-        return make_response(jsonify({'error': 'user is already added'}), 409)
-
-    try:
-        Session.add(req)
-        Session.commit()
-    except IntegrityError:
-        print('incorrect data')
-        return make_response(jsonify({'error': 'incorrect data'}), 409)
-    a = to_json(req, requestClass)
-    return Response(response=a,
-                status=200,
-                mimetype="application/json")
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+        schema.load(new_user_json)
+    except ValidationError as err:
+        return err.messages, 422
+    if s.query(User).filter(User.email == request.json.get('email')).first() is not None:
+        return {"message": "User with provided email already exists"}, 406
+    if s.query(User).filter(User.username == request.json.get('username')).first() is not None:
+        return {"message": "User with provided username already exists"}, 406
+    user_to_create = User(**new_user_json)
+    hashed_password = bcrypt.generate_password_hash(new_user_json['password'])
+    user_to_create.password = hashed_password
+    s.add(user_to_create)
+    s.commit()
+    return schema.dump(user_to_create), 200
